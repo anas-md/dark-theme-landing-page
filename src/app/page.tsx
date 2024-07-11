097264e9491703d8f687000959cea8889efbec91
Anas <anasdaud1996@gmail.com>
2024-07-12 01:02:36 +0800

    Setup the navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Banner } from '@/components/Banner';
+import { Navbar } from '@/components/Navbar';
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Banner />
+      <Navbar />
+    </>
+  );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,16 @@
 export const Banner = () => {
-  return null;
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,rgba(252,214,255,0.7),rgba(41,216,255,0.7),rgba(255,253,128,0.7),rgba(248,155,192,0.7),rgba(252,214,255,0.7))]">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden sm:inline">
+            Introducing a completely redesigned interface — {''}
+          </span>
+          <a href="#" className="underline underline-offset-4 ">
+            Explore the demo
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
