6cefb8639ec8e269b9197ad09afd39ebc62356ea
Anas <anasdaud1996@gmail.com>
2024-07-12 10:53:19 +0800

    Add Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Banner } from '@/components/Banner';
+import { Hero } from '@/components/Hero';
 import { Navbar } from '@/components/Navbar';
 export default function Home() {
   return (
     <>
       <Banner />
       <Navbar />
+      <Hero />
     </>
   );
 }
